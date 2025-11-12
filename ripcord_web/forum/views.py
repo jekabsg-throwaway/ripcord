2fab7982e9e9e2d6144d19bc89783a7e5af3fe72
jekabsg-throwaway <jekabsg1337@gmail.com>
2025-11-12 15:14:41 +0200

    set up static home page

--- ripcord_web/forum/views.py
+++ ripcord_web/forum/views.py
@@ -9,4 +9,3 @@ def post(request, post_id):
   post = get_object_or_404(Post, pk=post_id)
   comments = Comment.objects.filter(post=post)
   return render(request, 'forum/post.html', {'post': post, 'comments': comments})
-
