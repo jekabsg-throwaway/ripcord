d924e7b8fa49bc32c7ef66bf438804de7499bf6f
jekabsg-throwaway <jekabsg1337@gmail.com>
2025-11-12 14:12:45 +0200

    create basic forum index ui

--- ripcord_web/forum/views.py
+++ ripcord_web/forum/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
 def index(request):
-  return HttpResponse("forum index")
+  posts = Post.objects.all().order_by('-date')
+  return render(request, 'forum/index.html', {'posts': posts})
--- ripcord_web/ripcord_web/settings.py
+++ ripcord_web/ripcord_web/settings.py
@@ -121,3 +121,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1']
