3288482d0a6ef2a377274572df33c126737680de
jekabsg-throwaway <jekabsg1337@gmail.com>
2025-11-12 14:38:03 +0200

    implement post "read more" page

--- ripcord_web/forum/views.py
+++ ripcord_web/forum/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def index(request):
   posts = Post.objects.all().order_by('-date')
   return render(request, 'forum/index.html', {'posts': posts})
+
+def post(request, post_id):
+  post = get_object_or_404(Post, pk=post_id)
+  return render(request, 'forum/post.html', {'post': post})
+
