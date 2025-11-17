0d7154abbeb09ed10a13e760f8af416b3f255399
jekabsg-throwaway <jekabsg1337@gmail.com>
2025-11-17 21:28:30 +0200

    create api endpoint for forum posts

--- ripcord_web/forum/urls.py
+++ ripcord_web/forum/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 from . import views
+from .views import PostViewSet
+
+api_router = DefaultRouter()
+api_router.register(r'posts', PostViewSet)
 
 urlpatterns = [
   path("", views.index, name="index"),
   path("post/<int:post_id>", views.post, name="post"),
+  path("api/", include(api_router.urls)),
 ]
--- ripcord_web/forum/views.py
+++ ripcord_web/forum/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
+from rest_framework import viewsets
 from .models import Post, Comment
+from .serializers import PostSerializer
 
 def index(request):
   posts = Post.objects.all().order_by('-date')
@@ -9,3 +11,7 @@ def post(request, post_id):
   post = get_object_or_404(Post, pk=post_id)
   comments = Comment.objects.filter(post=post)
   return render(request, 'forum/post.html', {'post': post, 'comments': comments})
+
+class PostViewSet (viewsets.ModelViewSet):
+  queryset = Post.objects.all()
+  serializer_class = PostSerializer
